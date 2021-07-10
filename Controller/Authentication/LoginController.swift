aa6e70b314447828a5d7db2eb4403d791296420e
Nathan Yeo <yeo.nathan@outlook.com>
2021-07-11 02:03:19 +0800

    Completed up to tutorial 8

--- Controller/Authentication/LoginController.swift
+++ Controller/Authentication/LoginController.swift
@@ -16,27 +16,14 @@ class LoginController: UIViewController {
     return iv
   }()
 
-  private let emailTextField: UITextField = {
-    let tf = UITextField()
-    tf.borderStyle = .none
-    tf.textColor = . white
-    tf.keyboardAppearance = .dark
+  private let emailTextField: CustomTextField = {
+    let tf = CustomTextField(placeHolder: "Email")
     tf.keyboardType = .emailAddress
-    tf.backgroundColor = UIColor(white: 1, alpha: 0.1)
-    tf.setHeight(50)
-    tf.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
     return tf
   }()
 
-  private let passwordTextField: UITextField = {
-    let tf = UITextField()
-    tf.borderStyle = .none
-    tf.textColor = . white
-    tf.keyboardAppearance = .dark
-    tf.keyboardType = .emailAddress
-    tf.backgroundColor = UIColor(white: 1, alpha: 0.1)
-    tf.setHeight(50)
-    tf.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+  private let passwordTextField: CustomTextField = {
+    let tf = CustomTextField(placeHolder: "Password")
     tf.isSecureTextEntry = true
     return tf
   }()
@@ -54,22 +41,14 @@ class LoginController: UIViewController {
 
   private let forgotPasswordButton: UIButton = {
     let button = UIButton(type: .system)
-    let atts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.systemFont(ofSize: 16)]
-    let attributedTitle = NSMutableAttributedString(string: "Forgot your password?  ", attributes: atts)
-    let boldAtts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.boldSystemFont(ofSize: 16)]
-    attributedTitle.append(NSAttributedString(string: "Get help signing in", attributes: boldAtts))
-    button.setAttributedTitle(attributedTitle, for: .normal)
+    button.attributedTitle(normalText: "Forgot your password", boldedText: "Get help signing in")
     return button
   }()
 
 
   private let dontHaveAccountButton: UIButton = {
     let button = UIButton(type: .system)
-    let atts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.systemFont(ofSize: 16)]
-    let attributedTitle = NSMutableAttributedString(string: "Don't have an account?  ", attributes: atts)
-    let boldAtts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.boldSystemFont(ofSize: 16)]
-    attributedTitle.append(NSAttributedString(string: "Sign Up", attributes: boldAtts))
-    button.setAttributedTitle(attributedTitle, for: .normal)
+    button.attributedTitle(normalText: "Don't have an account?", boldedText: "Sign up")
     return button
   }()
 
--- Utils/Extensions.swift
+++ Utils/Extensions.swift
@@ -8,6 +8,16 @@
 
 import UIKit
 
+extension UIButton {
+  func attributedTitle(normalText: String, boldedText: String) {
+    let atts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.87), .font: UIFont.systemFont(ofSize: 16)]
+    let attributedTitle = NSMutableAttributedString(string: "\(normalText) ", attributes: atts)
+    let boldAtts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.87), .font: UIFont.boldSystemFont(ofSize: 16)]
+    attributedTitle.append(NSAttributedString(string: boldedText, attributes: boldAtts))
+    setAttributedTitle(attributedTitle, for: .normal)
+  }
+}
+
 extension UIView {
     func anchor(top: NSLayoutYAxisAnchor? = nil,
                 left: NSLayoutXAxisAnchor? = nil,
