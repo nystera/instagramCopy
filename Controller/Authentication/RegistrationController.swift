ad5bd5b30f31d0f88077b440d741090344a6b587
Nathan Yeo <yeo.nathan@outlook.com>
2021-07-11 01:41:33 +0800

    Completed up to tutorial video 7

---
+++ Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,20 @@
+//
+//  RegistrationController.swift
+//  InstagramCopy
+//
+//  Created by Nathan Yeo on 10/7/21.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+  // MARK: - Properties
+
+  // MARK: - Lifecycle
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    view.backgroundColor = .systemPink
+  }
+}
